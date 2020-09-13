a2201d84ea3a416fa0f585b1655de73908f5f44b
Bikal Lem <gbikal@gmail.com>
2020-09-13 12:19:25 +0100

    remove result, remove other dependencies

--- lib/reparse.mli
+++ lib/reparse.mli
@@ -16,138 +16,107 @@
 
 (** {2 Types} *)
 
-type error = [`Msg of string]
-(** Parser error type. *)
-
-type input =
-  [ `String    of string
-  | `Bigstring of Bigstringaf.t ]
-(** Represents parser input. *)
-
-type (+'a, +'error) t
+type +'a t
 (** The main parser type.['a] denotes the successful parse value while ['error]
     denotes error raised by the parser. *)
 
-val sexp_of_error : error -> Sexplib0.Sexp.t
+exception Parse_error of string
 
-val advance : int -> (unit, [> error]) t
+val return : 'a -> 'a t
+(** [return v] creates a new parser that always returns the constant [v]. *)
+
+val advance : int -> unit t
 (** [advance n] advances parser by the given [n] number of characters. Always
     succeeds. *)
 
-val end_of_input : (bool, [> error]) t
+val end_of_input : bool t
 (** [end_of_input] returns [true] if parser has reached end of input. Always
     succeeds. *)
 
 (** {2 Executing} *)
 
-val parse : input -> ('a, ([> error] as 'b)) t -> ('a, 'b) result
+val parse : string -> 'a t -> ('a, exn) result
 (** [parse input p] executes parser [p] with input [input]. *)
+
+(** {2 Combinators} *)
+
+val ( <|> ) : 'a t -> 'a t -> 'a t
+(** [p <|> q] creates a parser that executes [p] and returns the result if it is
+    successful. If false then it executes [q] and returns it. *)
+
+val ( >>= ) : 'a t -> ('a -> 'b t) -> 'b t
+(** [p >>= q] executes [p] and if it succeeds executes [q] and returns it's
+    result else it returns the result of executing [p]. *)
+
+val ( >|= ) : 'a t -> ('a -> 'b) -> 'b t
+(** [p >|= f] executes [p] and then if it is successful returns [a]. It then
+    executes [f a] and returns the result. *)
+
+val ( *> ) : _ t -> 'a t -> 'a t
+(** [p *> b] is [p >>= fun _ -> b] *)
+
+val ( <* ) : 'a t -> _ t -> 'a t
+(** [p <* q] discards result from [q] and returns [p] *)
 
 (** {2 Basic Parsers} *)
 
-val char : char -> (char, [> error]) t
+val char : char -> char t
 (** [char c] accepts character [c] from input exactly and returns it. Fails
     Otherwise.*)
 
-val char_if : (char -> bool) -> (char option, [> error]) t
+val char_if : (char -> bool) -> char option t
 (** [char_if f] accepts and returns [Some c] if [f c] is true. Otherwise it
     returns [None]. Always succeeds. *)
 
-val satisfy : (char -> bool) -> (char, [> error]) t
+val satisfy : (char -> bool) -> char t
 (** [satisfy f] accepts a char [c] from input if [f c] is true and returns it.
     Otherwise it fails. *)
 
-val peek_char : (char option, [> error]) t
+val peek_char : char option t
 (** [peek_char t] returns a character at the current position in the parser.
     Always suceeds and returns [None] if EOF is reached. *)
 
-val peek_char_fail : (char, [> error]) t
-(** Same as [peek_char] except the call fails if [end of input] is encountered. *)
-
-val any_char : (char, [> error]) t
-(** [any_char] accepts any char and returns it. Fails if EOF reached. *)
-
-val peek_string : int -> (string option, [> error]) t
+val peek_string : int -> string option t
 (** [peek_string n] attempts to match string of length [n] from input exactly
     and return it. If it isn't matched [None] is returned. *)
 
-val string : string -> (string, [> error]) t
-(** [string s] accepts [s] exactly and returns it. *)
+val string : string -> unit t
+(** [string s] accepts [s] exactly. *)
 
-val string_if : string -> (string option, [> error]) t
-(** [string_if s] accepts and returns [Some s] if [s] matches input. Otherwise
-    returns [None]. Always succeeds. *)
-
-val skip_while : (char -> bool) -> (unit, [> error]) t
+val skip_while : (char -> bool) -> unit t
 (** [skip_while f] keeps accepting [c] if [f c] is [true]. [c] is discarded.
     Always succeeds. *)
 
-val count_skip_while : (char -> bool) -> (int, [> error]) t
+val count_skip_while : (char -> bool) -> int t
 (** [count_skip_while f] accepts characters from input while [f c] is true and
     records the count of times the input char was accepted. The accepted chars
     are discarded and the count is returned. *)
 
-val count_skip_while_string : int -> (string -> bool) -> (int, [> error]) t
+val count_skip_while_string : int -> (string -> bool) -> int t
 (** [count_skip_while_string n f] accepts string [s] of length [n] if [f s] is
     true. The accepted string [s] is discarded and the count of times the string
     was accepted is noted. The count is then returned. *)
 
-val take_while : (char -> bool) -> (string, [> error]) t
+val take_while : (char -> bool) -> string t
 (** [take_while f] keeps accepting character [c] from input while [f c] is true.
     It then concatenates the accepted characters and converts it into a string
     and returns it. *)
 
-val take_while_n : int -> (char -> bool) -> (string, [> error]) t
+val take_while_n : int -> (char -> bool) -> string t
 (** [take_while_n n f] similar in functionality to [take_while]. The parser
     however has a maximum upper bound [n] on the number of characters it
     accepts. *)
 
-(** {2 Constructors} *)
-
-val ok : 'a -> ('a, [> error]) t
-(** [ok v] creates a new parser that always returns the constant [v]. *)
-
-val fail : ([> error] as 'e) -> (_, 'e) t
+(* val fail : ([> error] as 'e) -> (_, 'e) t *)
 (** [fail err] creates a parser that always fails with [err]. *)
 
-(** {2 Combinators} *)
-
-val ( <|> ) : ('a, 'error) t -> ('a, 'error) t -> ('a, 'error) t
-(** [p <|> q] creates a parser that executes [p] and returns the result if it is
-    successful. If false then it executes [q] and returns it. *)
-
-val ( >>= ) : ('a, 'error) t -> ('a -> ('b, 'error) t) -> ('b, 'error) t
-(** [p >>= q] executes [p] and if it succeeds executes [q] and returns it's
-    result else it returns the result of executing [p]. *)
-
-val ( >>| ) : ('a, 'error) t -> ('a -> 'b) -> ('b, 'error) t
-(** [p >>| f] executes [p] and then if it is successful returns [a]. It then
-    executes [f a] and returns the result. *)
-
-val ( >>|? ) : ('a, 'error) t -> ('error -> 'c) -> ('a, 'c) t
-(** [p >>|? f] executes [p] and maps [error] via [f error] if [p] results in
-    [error]. *)
-
-val ( >>*? ) : ('a, 'error) t -> 'f -> ('a, 'f) t
-(** [p >>*? e] executes [p] and returns error [e] discarding [error] if [p]
-    fails. *)
-
-val ( *> ) : (_, 'error) t -> ('a, 'error) t -> ('a, 'error) t
-(** [p *> b] is [p >>= fun _ -> b] *)
-
-val ( <* ) : ('a, 'error) t -> (_, 'error) t -> ('a, 'error) t
-(** [p <* q] discards result from [q] and returns [p] *)
-
-val ( *>| ) : (_, 'error) t -> 'a -> ('a, 'error) t
-(** [p *>| a] is [p >>| fun _ -> a] *)
-
-val many : ('a, [> error]) t -> ('a list, [> error]) t
+val many : 'a t -> 'a list t
 (** [many p] runs p zero or more times and returns a list of results from the
     runs of p.*)
 
-val count_skip_many : ('a, [> error]) t -> (int, [> error]) t
+val count_skip_many : 'a t -> int t
 (** [count_skip_many p] runs [p] zeor or more times *)
 
-val line : (string option, [> error]) t
+val line : string option t
 (** [line] accepts and returns a line of input delimited by either [\n] or
     [\r\n]. Returns [None] if end of input is reached. *)
