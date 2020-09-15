fa866d554c3ebb2a89cde382dc55e6eb39266c66
Bikal Lem <gbikal@gmail.com>
2020-09-15 13:33:19 +0100

    update test label

--- test/test_reparse.ml
+++ test/test_reparse.ml
@@ -3,18 +3,17 @@ open Reparse
 let test_lnum_cnum_is_1_11 () =
   let p = many next *> map2 (fun lnum cnum -> (lnum, cnum)) lnum cnum in
   let r = parse ~track_lnum:true "hello world" p in
-  Alcotest.(
-    check (result (pair int int) string) "line number: 1" (Ok (1, 11)) r)
+  Alcotest.(check (result (pair int int) string) "(1,11)" (Ok (1, 11)) r)
 
 let test_lnum_cnum_is_3_0 () =
   let p = many next *> map2 (fun lnum cnum -> (lnum, cnum)) lnum cnum in
   let r = parse ~track_lnum:true "hello world\nsecond line\n" p in
-  Alcotest.(check (result (pair int int) string) "line number: 2" (Ok (3, 0)) r)
+  Alcotest.(check (result (pair int int) string) "(3,0)" (Ok (3, 0)) r)
 
 let test_lnum_cnum_is_3_5 () =
   let p = many next *> map2 (fun lnum cnum -> (lnum, cnum)) lnum cnum in
   let r = parse ~track_lnum:true "hello world\nsecond line\naaaaa" p in
-  Alcotest.(check (result (pair int int) string) "line number: 2" (Ok (3, 5)) r)
+  Alcotest.(check (result (pair int int) string) "(3,5)" (Ok (3, 5)) r)
 
 let test_lnum_cnum_suite =
   [ ("test (lnum, cnum) is (1,11)", `Quick, test_lnum_cnum_is_1_11)
