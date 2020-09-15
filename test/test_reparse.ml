adcc4c448401225832d85cf254bf125423c467e7
Bikal Lem <gbikal@gmail.com>
2020-09-15 13:45:15 +0100

    add 0/0 lnum,cnum test

--- test/test_reparse.ml
+++ test/test_reparse.ml
@@ -15,10 +15,21 @@ let test_lnum_cnum_is_3_5 () =
   let r = parse ~track_lnum:true "hello world\nsecond line\naaaaa" p in
   Alcotest.(check (result (pair int int) string) "3/5" (Ok (3, 5)) r)
 
+let test_lnum_cnum_is_0_0 () =
+  let p = many next *> map2 (fun lnum cnum -> (lnum, cnum)) lnum cnum in
+  let r = parse "hello world\nsecond line\naaaaa" p in
+
+  Alcotest.(check (result (pair int int) string) "3/5" (Ok (0, 0)) r) ;
+
+  let p = many next *> map2 (fun lnum cnum -> (lnum, cnum)) lnum cnum in
+  let r = parse ~track_lnum:false "hello world\nsecond line\naaaaa" p in
+  Alcotest.(check (result (pair int int) string) "3/5" (Ok (0, 0)) r)
+
 let test_lnum_cnum_suite =
   [ ("1/11", `Quick, test_lnum_cnum_is_1_11)
   ; ("3/0", `Quick, test_lnum_cnum_is_3_0)
-  ; ("3/5", `Quick, test_lnum_cnum_is_3_5) ]
+  ; ("3/5", `Quick, test_lnum_cnum_is_3_5)
+  ; ("0/0", `Quick, test_lnum_cnum_is_0_0) ]
 
 let () =
   Printexc.record_backtrace true ;
