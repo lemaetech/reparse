6297883340f2db7ca635778d7249e89f2c8dfe61
Bikal Lem <gbikal@gmail.com>
2020-09-16 12:51:48 +0100

    remove next.ml

--- test/next.ml
+++
@@ -1,8 +0,0 @@
-open Reparse
-
-let test_next () =
-  let p = map2 (fun c o -> (c, o)) next offset in
-  let r = parse "hello" p in
-  Alcotest.(check (pair char int) "'h',1" ('h', 1) r)
-
-let suite = [("next is ('h', 1)", `Quick, test_next)]
