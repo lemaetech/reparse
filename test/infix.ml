18b2c9ffa418befe39b4e1b393b839bbc30af08b
Bikal Lem <gbikal@gmail.com>
2020-09-16 12:30:34 +0100

    add test for <*>

--- test/infix.ml
+++ test/infix.ml
@@ -10,4 +10,12 @@ let test_bind () =
   let r = parse "hello" p in
   Alcotest.(check int "104" 104 r)
 
-let suite = [(">>= ", `Quick, test_bind); (">|= ", `Quick, test_map)]
+let test_applicative () =
+  let c = return (fun a -> a + 2) <*> return 2 in
+  let r = parse "" c in
+  Alcotest.(check int "4" 4 r)
+
+let suite =
+  [ (">>= ", `Quick, test_bind)
+  ; (">|= ", `Quick, test_map)
+  ; ("<*>", `Quick, test_applicative) ]
