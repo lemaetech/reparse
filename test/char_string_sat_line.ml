4b6a993e228b5733f07a8d880a0c304b1dd69297
Bikal Lem <gbikal@gmail.com>
2020-09-30 23:29:33 +0100

    test file io for char_string_sat.ml

--- test/char_string_sat_line.ml
+++ test/char_string_sat_line.ml
@@ -1,15 +1,13 @@
-module P = Reparse.Parse.String_parser
+module M = Make_test
 
-let src = Reparse.Io.String.create
-
-let char_h () =
+let char_h (type s) (module P : M.S with type io = s) src () =
   let p = P.map2 (fun c o -> (c, o)) (P.char 'h') P.offset in
-  let r = P.parse (src "hello") p in
+  let r = P.parse src p in
   Alcotest.(check (pair char int) "'h', 1" ('h', 1) r)
 
-let char_exn () =
+let char_exn (type s) (module P : M.S with type io = s) src () =
   let p = P.char 'h' in
-  let r () = ignore (P.parse (src "aaaa") p) in
+  let r () = ignore (P.parse src p) in
   Alcotest.(
     check_raises
       "char"
@@ -20,14 +18,14 @@ let char_exn () =
          ; msg = "[char] expected 'h'" })
       r)
 
-let string () =
+let string (type s) (module P : M.S with type io = s) src () =
   let p = P.map2 (fun s o -> (s, o)) (P.string "hello") P.offset in
-  let r = P.parse (src "hello") p in
+  let r = P.parse src p in
   Alcotest.(check (pair string int) "\"hello\", 4" ("hello", 5) r)
 
-let string_exn () =
+let string_exn (type s) (module P : M.S with type io = s) src () =
   let p = P.string "hello" in
-  let r () = ignore (P.parse (src "world") p) in
+  let r () = ignore (P.parse src p) in
   Alcotest.(
     check_raises
       "string"
@@ -42,14 +40,14 @@ let is_char = function
       true
   | _ -> false
 
-let satisfy () =
+let satisfy (type s) (module P : M.S with type io = s) src () =
   let p = P.satisfy is_char in
-  let r = P.parse (src "a") p in
+  let r = P.parse src p in
   Alcotest.(check char "a" 'a' r)
 
-let satisfy_exn () =
+let satisfy_exn (type s) (module P : M.S with type io = s) src () =
   let p = P.satisfy is_char in
-  let r () = ignore (P.parse (src "d") p) in
+  let r () = ignore (P.parse src p) in
   Alcotest.(
     check_raises
       "satisfy"
@@ -58,9 +56,15 @@ let satisfy_exn () =
       r)
 
 let suite =
-  [ ("char ", `Quick, char_h)
-  ; ("char exn", `Quick, char_exn)
-  ; ("string", `Quick, string)
-  ; ("string exn", `Quick, string_exn)
-  ; ("satisfy", `Quick, satisfy)
-  ; ("satisfy exn", `Quick, satisfy_exn) ]
+  [ M.make_string_test "char " char_h "hello"
+  ; M.make_string_test "char exn" char_exn "aaaa"
+  ; M.make_string_test "string" string "hello"
+  ; M.make_string_test "string exn" string_exn "world"
+  ; M.make_string_test "satisfy" satisfy "a"
+  ; M.make_string_test "satisfy exn" satisfy_exn "d"
+  ; M.make_file_test "char " char_h "hello"
+  ; M.make_file_test "char exn" char_exn "aaaa"
+  ; M.make_file_test "string" string "hello"
+  ; M.make_file_test "string exn" string_exn "world"
+  ; M.make_file_test "satisfy" satisfy "a"
+  ; M.make_file_test "satisfy exn" satisfy_exn "d" ]
---
+++ test/make_test.ml
@@ -0,0 +1,22 @@
+module type S = Reparse.Parse_sig.S
+
+type 's test =
+  (module Reparse.Parse_sig.S with type io = 's) -> 's -> unit -> unit
+
+let () = Random.self_init ()
+
+let make_string_test test_name (test : 's test) test_data =
+  let src = Reparse.Io.String.create test_data in
+  ( "[String] " ^ test_name
+  , `Quick
+  , test (module Reparse.Parse.String_parser) src )
+
+let make_file_test test_name (test : 's test) test_data =
+  let make_file content =
+    let fname = string_of_int @@ Random.bits () in
+    let fd = Unix.openfile fname [Unix.O_RDWR; Unix.O_CREAT] 0o640 in
+    let _w = Unix.write_substring fd content 0 (String.length content) in
+    fd
+  in
+  let src = make_file test_data |> Reparse.Io.File.create in
+  ("[File]   " ^ test_name, `Quick, test (module Reparse.Parse.File_parser) src)
