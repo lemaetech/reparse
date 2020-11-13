ed05b571f8b05fec88b8288588be1e4ac7709d55
Bikal Lem <gbikal@gmail.com>
2020-11-13 00:28:29 +0000

    update test

--- test/infix.ml
+++ test/infix.ml
@@ -1,26 +1,28 @@
-module P = Reparse.String_parser
+module P = Reparse.Parser
 open P.Infix
-
-let src = Reparse.String_input.create
 
 let test_map () =
   let p = P.peek_char >|= fun c -> Char.code c in
-  let r = P.parse (src "hello") p in
+  let input = new P.string_input "hello" in
+  let r = P.parse input p in
   Alcotest.(check int "104" 104 r)
 
 let test_bind () =
   let p = P.peek_char >>= fun c -> P.return (Char.code c) in
-  let r = P.parse (src "hello") p in
+  let input = new P.string_input "hello" in
+  let r = P.parse input p in
   Alcotest.(check int "104" 104 r)
 
 let test_applicative () =
   let c = P.return (fun a -> a + 2) <*> P.return 2 in
-  let r = P.parse (src "") c in
+  let input = new P.string_input "" in
+  let r = P.parse input c in
   Alcotest.(check int "4" 4 r)
 
-let test_less_dollar (* <$ *) () =
+let test_less_dollar () =
   let p = 4 <$ P.char 'h' in
-  let r = P.parse (src "hello") p in
+  let input = new P.string_input "hello" in
+  let r = P.parse input p in
   Alcotest.(check int "4" 4 r)
 
 let suite =
