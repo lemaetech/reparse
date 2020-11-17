ecb89e5f008fba2228bf57a8b51424c5c8c4f231
Bikal Lem <gbikal@gmail.com>
2020-11-17 15:32:27 +0000

    add json example

--- lib/parser.mli
+++ lib/parser.mli
@@ -33,89 +33,9 @@
     - Text
     - RFC 5234 core parsers
 
-    An {{:#infix} Infix} module contains infix and let syntax support functions. *)
+    An {{:#infix} Infix} module contains infix and let syntax support functions.
 
-(** {2 Example - calculator} *)
-
-(** An example calculator that supports [+,-,*] and [/] calculations.
-
-    The expression grammar is defined by the following BNF grammar:
-
-    {v
-<expr>   ::= <term>   "+" <expr> 
-           | <term>
-<term>   ::= <factor> "*" <term> 
-           | <factor>
-<factor> ::= "(" <expr> ")" 
-           | integer
-    v}
-    {[
-      module P = Reparse.Parser
-      open P.Infix
-
-      type expr =
-        | Int  of int
-        | Add  of expr * expr
-        | Sub  of expr * expr
-        | Mult of expr * expr
-        | Div  of expr * expr
-
-      let skip_spaces = P.skip P.space
-
-      let binop : 'a P.t -> char -> 'b P.t -> ('a -> 'b -> 'c) -> 'c P.t =
-       fun exp1 op exp2 f ->
-        P.map3
-          (fun e1 _ e2 -> f e1 e2)
-          exp1
-          (skip_spaces *> P.char op <* skip_spaces)
-          exp2
-
-      let integer : expr P.t =
-        let+ d = P.digits in
-        Int (int_of_string d)
-
-      let factor : expr P.t -> expr P.t =
-       fun expr ->
-        P.any
-          [ P.char '(' *> skip_spaces *> expr <* skip_spaces <* P.char ')'
-          ; skip_spaces *> integer <* skip_spaces ]
-
-      let term : expr P.t -> expr P.t =
-       fun factor ->
-        P.fix (fun term ->
-            let mult = binop factor '*' term (fun e1 e2 -> Mult (e1, e2)) in
-            let div = binop factor '/' term (fun e1 e2 -> Div (e1, e2)) in
-            mult <|> div <|> factor)
-
-      let expr : expr P.t =
-        P.fix (fun expr ->
-            let factor = factor expr in
-            let term = term factor in
-            let add = binop term '+' expr (fun e1 e2 -> Add (e1, e2)) in
-            let sub = binop term '-' expr (fun e1 e2 -> Sub (e1, e2)) in
-            P.any [add; sub; term])
-
-      let rec eval : expr -> int = function
-        | Int i         -> i
-        | Add (e1, e2)  -> eval e1 + eval e2
-        | Sub (e1, e2)  -> eval e1 - eval e2
-        | Mult (e1, e2) -> eval e1 * eval e2
-        | Div (e1, e2)  -> eval e1 / eval e2
-
-      let parse : string -> expr =
-       fun s ->
-        let input = new P.string_input s in
-        P.parse input expr
-
-      (* Test AST *)
-      let r =
-        let actual = parse "1*2-4+3" in
-        let expected = Sub (Mult (Int 1, Int 2), Add (Int 4, Int 3)) in
-        Bool.equal (expected = actual) true
-
-      (* Run the evaluator. *)
-      let exp_result = eval (parse "12+1*10") |> Int.equal 22
-    ]} *)
+    See {{:#examples} examples} of use.*)
 
 (** {2 Parser type} *)
 
@@ -1647,3 +1567,253 @@ module Infix : sig
         r = 10
       ]} *)
 end
+
+(** {1:examples Examples} *)
+
+(** {2 Calculator} *)
+
+(** An example calculator that supports [+,-,*] and [/] calculations.
+
+    The expression grammar is defined by the following BNF grammar:
+
+    {v
+<expr>   ::= <term>   "+" <expr> 
+           | <term>
+<term>   ::= <factor> "*" <term> 
+           | <factor>
+<factor> ::= "(" <expr> ")" 
+           | integer
+    v}
+    {[
+      module P = Reparse.Parser
+      open P.Infix
+
+      type expr =
+        | Int  of int
+        | Add  of expr * expr
+        | Sub  of expr * expr
+        | Mult of expr * expr
+        | Div  of expr * expr
+
+      let skip_spaces = P.skip P.space
+
+      let binop : 'a P.t -> char -> 'b P.t -> ('a -> 'b -> 'c) -> 'c P.t =
+       fun exp1 op exp2 f ->
+        P.map3
+          (fun e1 _ e2 -> f e1 e2)
+          exp1
+          (skip_spaces *> P.char op <* skip_spaces)
+          exp2
+
+      let integer : expr P.t =
+        let+ d = P.digits in
+        Int (int_of_string d)
+
+      let factor : expr P.t -> expr P.t =
+       fun expr ->
+        P.any
+          [ P.char '(' *> skip_spaces *> expr <* skip_spaces <* P.char ')'
+          ; skip_spaces *> integer <* skip_spaces ]
+
+      let term : expr P.t -> expr P.t =
+       fun factor ->
+        P.fix (fun term ->
+            let mult = binop factor '*' term (fun e1 e2 -> Mult (e1, e2)) in
+            let div = binop factor '/' term (fun e1 e2 -> Div (e1, e2)) in
+            mult <|> div <|> factor)
+
+      let expr : expr P.t =
+        P.fix (fun expr ->
+            let factor = factor expr in
+            let term = term factor in
+            let add = binop term '+' expr (fun e1 e2 -> Add (e1, e2)) in
+            let sub = binop term '-' expr (fun e1 e2 -> Sub (e1, e2)) in
+            P.any [add; sub; term])
+
+      let rec eval : expr -> int = function
+        | Int i         -> i
+        | Add (e1, e2)  -> eval e1 + eval e2
+        | Sub (e1, e2)  -> eval e1 - eval e2
+        | Mult (e1, e2) -> eval e1 * eval e2
+        | Div (e1, e2)  -> eval e1 / eval e2
+
+      let parse : string -> expr =
+       fun s ->
+        let input = new P.string_input s in
+        P.parse input expr
+
+      (* Test AST *)
+      let r =
+        let actual = parse "1*2-4+3" in
+        let expected = Sub (Mult (Int 1, Int 2), Add (Int 4, Int 3)) in
+        Bool.equal (expected = actual) true
+
+      (* Run the evaluator. *)
+      let exp_result = eval (parse "12+1*10") |> Int.equal 22
+    ]} *)
+
+(** {2 Json} *)
+
+(** Implements JSON parser as defined in https://tools.ietf.org/html/rfc8259.
+
+    Assumes UTF-8 character encoding. However, it doesn't do any validation.
+
+    Sample top_level inputs;
+
+    {v
+  parse json_value "true";;
+  parse json_value "false";;
+  parse json_value "null";;
+  parse json_value "123";;
+  parse json_value "123.345";;
+  parse json_value "123e123";;
+  parse json_value "123.33E123";;
+  parse json_value {|{"field1": 123,"field2": "value2"}|};;
+  parse json_value {|{"field1":[123,"hello",-123.23], "field2":123} |};;
+  parse json_value {|{"field1":123, "field2":123} |};;
+  parse json_value {|[123,"hello",-123.23, 123.33e13, 123E23] |};;
+    v}
+    {[
+      module P = Reparse.Parser
+      open P.Infix
+
+      type value =
+        | Object of (string * value) list
+        | Array  of value list
+        | Number of
+            { negative : bool
+            ; int : string
+            ; frac : string option
+            ; exponent : string option }
+        | String of string
+        | False
+        | True
+        | Null
+
+      let ws =
+        P.skip
+          (P.satisfy (function
+              | ' '
+              | '\t'
+              | '\n'
+              | '\r' ->
+                  true
+              | _ -> false))
+
+      let implode l = List.to_seq l |> String.of_seq
+      let struct_char c = ws *> P.char c <* ws
+      let null_value = ws *> P.string "null" *> ws *> P.pure Null
+      let false_value = ws *> P.string "false" *> ws *> P.pure False
+      let true_value = ws *> P.string "true" *> ws *> P.pure True
+      let sprintf = Printf.sprintf
+
+      let number_value =
+        let* negative =
+          P.optional (P.char '-')
+          >|= function
+          | Some '-' -> true
+          | _        -> false
+        in
+        let* int =
+          let digits1_to_9 =
+            P.satisfy (function
+                | '1' .. '9' -> true
+                | _          -> false)
+          in
+          let num =
+            P.map2
+              (fun first_ch digits -> sprintf "%c%s" first_ch digits)
+              digits1_to_9
+              P.digits
+          in
+          P.any [P.string "0"; num]
+        in
+        let* frac = P.optional (P.char '.' *> P.digits) in
+        let+ exponent =
+          P.optional
+            (let* e = P.char 'E' <|> P.char 'e' in
+             let* sign = P.optional (P.char '-' <|> P.char '+') in
+             let sign =
+               match sign with
+               | Some c -> sprintf "%c" c
+               | None   -> ""
+             in
+             let+ digits = P.digits in
+             sprintf "%c%s%s" e sign digits)
+        in
+        Number {negative; int; frac; exponent}
+
+      let string =
+        let escaped =
+          let ch =
+            P.char '\\'
+            *> P.satisfy (function
+                   | '"'
+                   | '\\'
+                   | '/'
+                   | 'b'
+                   | 'f'
+                   | 'n'
+                   | 'r'
+                   | 't' ->
+                       true
+                   | _ -> false)
+            >|= sprintf "\\%c"
+          in
+          let hex4digit =
+            let+ hex =
+              P.string "\\u" *> P.take ~at_least:4 ~up_to:4 P.hex_digit
+              >|= implode
+            in
+            sprintf "\\u%s" hex
+          in
+          P.any [ch; hex4digit]
+        in
+        let unescaped =
+          P.take_while
+            ~while_:(P.is_not (P.any [P.char '\\'; P.control; P.dquote]))
+            P.next
+          >|= implode
+        in
+        let+ str =
+          P.dquote *> P.take (P.any [escaped; unescaped]) <* P.dquote
+        in
+        String.concat "" str
+
+      let string_value = string >|= fun s -> String s
+
+      let json_value =
+        P.fix (fun value ->
+            let value_sep = struct_char ',' in
+            let object_value =
+              let member =
+                let* nm = string <* struct_char ':' in
+                let+ v = value in
+                (nm, v)
+              in
+              let+ object_value =
+                struct_char '{' *> P.take member ~sep_by:value_sep
+                <* struct_char '}'
+              in
+              Object object_value
+            in
+            let array_value =
+              let+ vals =
+                struct_char '[' *> P.take value ~sep_by:value_sep
+                <* struct_char ']'
+              in
+              Array vals
+            in
+            P.any
+              [ object_value
+              ; array_value
+              ; number_value
+              ; string_value
+              ; false_value
+              ; true_value
+              ; null_value ])
+
+      let parse p s =
+        let input = new P.string_input s in
+        P.parse input p
+    ]} *)
