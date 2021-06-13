801390666202c6be3a8f86616b11f30228bd1a3e
Bikal Lem <gbikal@gmail.com>
2021-06-13 22:18:03 +0100

    test alt

--- tests/run.ml
+++ tests/run.ml
@@ -1,1 +1,6 @@
-let () = Popper.suite [ ("String parsers", String_parsers.suite) ] |> Popper.run
+let () =
+  Popper.suite
+    [ ("String parsers", Test_string_parsers.suite)
+    ; ("Alternative parsers", Test_alternative_parsers.suite)
+    ]
+  |> Popper.run
