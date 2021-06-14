9e5471a62b0f95f915b487b95c011c3e88c0f27e
Bikal Lem <gbikal@gmail.com>
2021-06-14 12:56:00 +0100

    add boolean parsers test

---
+++ tests/test_boolean_parsers.ml
@@ -0,0 +1,35 @@
+module Make_test (P : Test_parser.TEST_PARSER) = struct
+  open Test_parser.Make_helper (P)
+
+  open P.Infix
+
+  let not_ =
+    let p = P.(not_ (string "hello")) in
+    let p2 = P.(not_ (string "world")) in
+    let inp () = P.of_string "world" in
+    Popper.(
+      suite
+        [ ( "value is ()"
+          , test (fun () -> equal unit_result_comparator (P.run p inp) (Ok ()))
+          )
+        ; pos_test p 0 inp
+        ; committed_pos_test p 0 inp
+        ; ( "fail"
+          , test (fun () ->
+                equal unit_result_comparator (P.run p2 inp)
+                  (Error "[not_] expected failure but succeeded")) )
+        ; ( "pos is error"
+          , test (fun () ->
+                let p = p2 *> P.pos in
+                equal int_result_comparator (P.run p inp)
+                  (Error "[not_] expected failure but succeeded")) )
+        ])
+
+  let suites =
+    Popper.suite [ ("not_", not_) (* ("is", is); ("is_not", is_not) *) ]
+end
+
+let suite =
+  let module S = Make_test (Test_parser.String) in
+  let module L = Make_test (Test_parser.Lwt) in
+  Popper.suite [ ("Reparse.String", S.suites); ("Lwt.Stream", S.suites) ]
