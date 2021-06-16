50b48dbdd1fd4383a427f26fdfacadb8e769c2b1
Bikal Lem <gbikal@gmail.com>
2021-06-16 12:44:22 +0100

    refactor common functionality in reparse-lwt

--- lib-lwt/reparse_lwt.ml
+++ lib-lwt/reparse_lwt.ml
@@ -44,68 +44,63 @@ module Stream = struct
       t.last_trimmed_pos <- pos;
       Lwt.return_unit
 
-    let get_char t ~pos =
-      let len = 1 in
+    let buffer_pos_len t ~pos ~len =
       let pos' = pos - t.last_trimmed_pos in
       let len' = Cstruct.length t.buf - (pos' + len) in
+      (pos', len')
+
+    let get_char_common t ~pos =
+      let pos', len' = buffer_pos_len t ~pos ~len:1 in
       if len' >= 0 then
-        Lwt.return (`Char (Cstruct.get_char t.buf pos'))
+        Lwt.return (`Char (Cstruct.get_char t.buf pos'), `Buf_not_exceeded)
       else
         Lwt_stream.get t.stream
         >|= function
-        | Some c ->
-          let s1 = Cstruct.create_unsafe (Cstruct.length t.buf + 1) in
-          Cstruct.blit t.buf 0 s1 0 (Cstruct.length t.buf);
-          Cstruct.set_char s1 (Cstruct.length s1 - 1) c;
-          t.buf <- s1;
-          `Char c
-        | None -> `Eof
+        | Some c -> (`Char c, `Buf_exceeded)
+        | None -> (`Eof, `Buf_not_exceeded)
+
+    let get_char t ~pos =
+      get_char_common t ~pos
+      >|= function
+      | `Char c, `Buf_not_exceeded -> `Char c
+      | `Char c, `Buf_exceeded ->
+        let s1 = Cstruct.create_unsafe (Cstruct.length t.buf + 1) in
+        Cstruct.blit t.buf 0 s1 0 (Cstruct.length t.buf);
+        Cstruct.set_char s1 (Cstruct.length s1 - 1) c;
+        t.buf <- s1;
+        `Char c
+      | `Eof, _ -> `Eof
 
     let get_char_unbuffered t ~pos =
-      let len = 1 in
-      let pos' = pos - t.last_trimmed_pos in
-      let len' = Cstruct.length t.buf - (pos' + len) in
+      get_char_common t ~pos
+      >|= function
+      | `Char c, _ -> `Char c
+      | `Eof, _ -> `Eof
+
+    let get_cstruct_common t ~pos ~len =
+      let pos', len' = buffer_pos_len t ~pos ~len in
       if len' >= 0 then
-        Lwt.return (`Char (Cstruct.get_char t.buf pos'))
+        Lwt.return (`Cstruct (Cstruct.sub t.buf pos' len), `Buf_not_exceeded)
       else
-        Lwt_stream.get t.stream
-        >|= function
-        | Some c -> `Char c
-        | None -> `Eof
+        Lwt_stream.nget (abs len') t.stream
+        >|= fun chars ->
+        let s1 = chars |> List.to_seq |> String.of_seq |> Cstruct.of_string in
+        (`Cstruct s1, `Buf_exceeded (pos', abs len'))
 
     let get_cstruct_unbuffered t ~pos ~len =
-      let pos' = pos - t.last_trimmed_pos in
-      let len' = Cstruct.length t.buf - (pos' + len) in
-      if len' >= 0 then
-        Lwt.return (`Cstruct (Cstruct.sub t.buf pos' len))
-      else
-        Lwt_stream.nget (abs len') t.stream
-        >|= fun chars ->
-        let s1 =
-          chars
-          |> List.to_seq
-          |> String.of_seq
-          |> Cstruct.of_string
-          |> Cstruct.append (Cstruct.sub t.buf pos' len)
-        in
-        `Cstruct s1
+      get_cstruct_common t ~pos ~len
+      >|= function
+      | `Cstruct buf, `Buf_not_exceeded -> `Cstruct buf
+      | `Cstruct buf, `Buf_exceeded (pos', len) ->
+        `Cstruct (Cstruct.append (Cstruct.sub t.buf pos' len) buf)
 
     let get_cstruct t ~pos ~len =
-      let pos' = pos - t.last_trimmed_pos in
-      let len' = Cstruct.length t.buf - (pos' + len) in
-      if len' >= 0 then
-        Lwt.return (`Cstruct (Cstruct.sub t.buf pos' len))
-      else
-        Lwt_stream.nget (abs len') t.stream
-        >|= fun chars ->
-        let s1 =
-          chars
-          |> List.to_seq
-          |> String.of_seq
-          |> Cstruct.of_string
-          |> Cstruct.append t.buf
-        in
-        t.buf <- s1;
+      get_cstruct_common t ~pos ~len
+      >|= function
+      | `Cstruct buf, `Buf_not_exceeded -> `Cstruct buf
+      | `Cstruct buf, `Buf_exceeded (pos', len) ->
+        let new_buf = Cstruct.append t.buf buf in
+        t.buf <- new_buf;
         `Cstruct (Cstruct.sub t.buf pos' len)
 
     let last_trimmed_pos t = return t.last_trimmed_pos
