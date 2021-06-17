3c4c69bcc7fa519fcbf0776f8d87d34d9d753c43
Bikal Lem <gbikal@gmail.com>
2021-06-17 17:18:09 +0100

    add input parsers tests

--- tests/run.ml
+++ tests/run.ml
@@ -5,5 +5,6 @@ let () =
     [ ("String parsers", Test_string_parsers.suite)
     ; ("Alternative parsers", Test_alternative_parsers.suite)
     ; ("Boolean parsers", Test_boolean_parsers.suite)
+    ; ("Input parsers", Test_input_parsers.suite)
     ]
   |> Popper.run
